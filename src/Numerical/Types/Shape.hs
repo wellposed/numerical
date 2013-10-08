69cc8e0b687334827e113086e70b8861ad49cafa
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-10-08 16:26:52 -0400

    type system hackery for reversing shapes / indices

--- src/Numerical/Types/Shape.hs
+++ src/Numerical/Types/Shape.hs
@@ -9,23 +9,30 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 --{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Numerical.Types.Shape where
 
 
 import Data.Data
+import Unsafe.Coerce
+
+--- at some point, I should evaluate  using the >= 7.8 type level nats 
+--- also this nat type should be perhaps in its own module for sanity reasons
+data PNat = S !PNat  | Z 
+
 
 
 infixr 3 :*
-
+    
  
-data Shape (sh :: [*]) where 
-    Nil  :: Shape ( '[] )
-    (:*) :: !(a:: *) -> !(Shape sh) -> Shape (a ': sh)
+data Shape (rank :: PNat) where 
+    Nil  :: Shape Z
+    (:*) :: {-# UNPACK #-} !(Int:: *) -> !(Shape r) -> Shape ( (S r))
 
 {-|
-index ideas inspired by repa3 / repa4
+index ideas inspired by repa3 / repa4, but 
 
 NOTE: for the the low rank cases (eg 1-2 dim arrays)
 should figure out a lighter weight indexing story if that
@@ -36,15 +43,35 @@ heavy way will be a bad bad idea.
 
 -} 
 
--- doing the rank2 tranpose only, because transpose only really makes sense
--- in the 2d case anyways. 
---tranposeIndex :: IntShape sh => (Int :* Int :* sh) -> ( Int :* Int :* sh )
---tranposeIndex (i :* j :* rs) = (j:* i :* rs)
---{-# INLINE tranposeIndex#-}
+-- using unsafe coerce as a ghetto way of writing the "proof"
+-- this can be made properly type safe if i switch to 7.8 support only
+-- that said
+
+-- reverse an Index Shape tuple    
+reverseShape :: Shape r -> Shape r 
+reverseShape Nil = Nil 
+reverseShape shs@(anIx :* rest) = go  shs Nil 
+    where
+        go :: Shape a -> Shape b -> Shape r  -- want r= PSum a b
+        go Nil res =  unsafeCoerce $!  res 
+        go (ix :* more )  res =  unsafeCoerce $! go more (ix :* res)
+{-# INLINABLE reverseShape #-}
 
 
+--shapePrinciple :: (Shape Z -> Shape tot -> Shape tot)->(Shape (S h))
 
+--type family PSum (a :: PNat) (b :: PNat) :: PNat
 
+--type instance PSum  Z a = a
+--type instance PSum (S a) b = S (PSum a b)
+-- the proper Summation type needs to also have
+--type instance  Psum  a (S b) = S (PSum a b)
+--- but that requires closed type families
+
+type Zero = Z 
+type One = S Zero
+type Two = S One
+type Three = S Two 
 
 
 
@@ -53,9 +80,9 @@ heavy way will be a bad bad idea.
 newtype Shaped lay sh = Shaped sh 
 
 -- |  Writing down the common ranks. 
-type DIM1 = Shape '[Int] 
-type DIM2 = Shape [Int,Int]
-type DIM3 = Shape [Int,Int,Int] 
+type DIM1 = Shape  One
+type DIM2 = Shape  Two 
+type DIM3 = Shape Three 
 
 
 {-| 'Shape' is our way of typing our hlist tuples, rulling out eg 
