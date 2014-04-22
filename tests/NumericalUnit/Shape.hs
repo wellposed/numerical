7badd2ca40579603cf933709b894af98cbb29302
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-04-22 15:13:04 -0400

    test case for shape foldr

--- tests/NumericalUnit/Shape.hs
+++ tests/NumericalUnit/Shape.hs
@@ -15,6 +15,6 @@ import Test.Tasty.HUnit
 
 unitTestShape = testGroup "Shape Unit tests"
     [ testCase "foldl on shape" $ ( S.foldl (+) 0 (1:* 2:* 3 :* Nil )  @?=  ( P.foldl   (+) 0  [1,2,3])  )
-
+    , testCase "foldr on shape" $ ( S.foldr (+) 0 (1:* 2:* 3 :* Nil )  @?=  ( P.foldr  (+) 0  [1,2,3])  )
 
     ]
