a6a717206fe924acebceb2575ed05d3537420468
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-04-22 16:17:05 -0400

    basic testing for scanl and scanr on shape

--- tests/NumericalUnit/Shape.hs
+++ tests/NumericalUnit/Shape.hs
@@ -16,5 +16,6 @@ import Test.Tasty.HUnit
 unitTestShape = testGroup "Shape Unit tests"
     [ testCase "foldl on shape" $ ( S.foldl (+) 0 (1:* 2:* 3 :* Nil )  @?=  ( P.foldl   (+) 0  [1,2,3])  )
     , testCase "foldr on shape" $ ( S.foldr (+) 0 (1:* 2:* 3 :* Nil )  @?=  ( P.foldr  (+) 0  [1,2,3])  )
-
+    , testCase "scanr1 on shape" (S.scanr1 (+) 0 (1:* 1 :* 1:* Nil )   @?=  (3:* 2:* 1 :* Nil ) )
+    , testCase "scanl1 on shape" (S.scanl1 (+) 0 (1:* 1 :* 1:* Nil )   @?=  (1:* 2:* 3:* Nil ) )
     ]
