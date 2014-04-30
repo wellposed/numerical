75e9af51635df87c9c0a4476cfb791258d695213
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-04-29 23:51:58 -0400

    reorganize group of unit tests

--- tests/NumericalUnit/Shape.hs
+++ tests/NumericalUnit/Shape.hs
@@ -17,17 +17,23 @@ import Test.Tasty.HUnit
 unitTestShape = testGroup "Shape Unit tests"
         [ testCase "foldl on shape" $  S.foldl (+) 0 (1:* 2:* 3 :* Nil )  @?=   P.foldl   (+) 0  [1,2,3]  
         , testCase "foldr on shape" $  S.foldr (+) 0 (1:* 2:* 3 :* Nil )  @?=   P.foldr  (+) 0  [1,2,3]  
+        , testCase "foldl1 on shape" $  S.foldl1 (+) (1:* 2:* 3 :* Nil )  @?=   P.foldl1   (+)  [1,2,3]  
+        , testCase "foldr1 on shape" $  S.foldr1 (+) (1:* 2:* 3 :* Nil )  @?=   P.foldr1  (+)  [1,2,3]  
+
         , testCase "scanr1 on shape" $ S.scanr1 (+) 0 (1:* 1 :* 1:* Nil )   @?=  (3:* 2:* 1 :* Nil ) 
         , testCase "scanl1 on shape" $ S.scanl1 (+) 0 (1:* 1 :* 1:* Nil )   @?=  (1:* 2:* 3:* Nil ) 
         , testCase "scanr1 on shape" $ S.scanr (+) 0 (1:* 1 :* 1:* Nil )   @?=  (3:* 2:* 1 :* 0:* Nil ) 
         , testCase "scanl1 on shape" $ S.scanl (+) 0 (1:* 1 :* 1:* Nil )   @?=  (0 :* 1:* 2:* 3:* Nil )     
-        , testCase "shapeToList on shape" $  S.shapeToList (1:* 2 :* 3 :* Nil) @?= [1,2,3]  
         , testCase "unsnoc on shape " $ (snd . S.unsnoc) (1:*2 :* Nil ) @?= 2 
         , testCase "snoc cons  uncons" $ (S.shapeToList . flip  snoc 1 . cons 1 )  Nil  @?= [1,1]
-        , testCase "foldl1 on shape" $  S.foldl1 (+) (1:* 2:* 3 :* Nil )  @?=   P.foldl1   (+)  [1,2,3]  
-        , testCase "foldr1 on shape" $  S.foldr1 (+) (1:* 2:* 3 :* Nil )  @?=   P.foldr1  (+)  [1,2,3]  
+
+
+        , testCase "shapeToList on shape" $  S.shapeToList (1:* 2 :* 3 :* Nil) @?= [1,2,3]  
+
+
         , testCase "Show on Nil shape" $ show Nil @?= "Nil"
         , testCase "Show on 1:* Nil"  $ show (1:* Nil) @?= "1 :* Nil" 
+
         , testCase "storable on size 0 shape" $ 
             do a <- return (svFromList [Nil,Nil :: Shape Z Int]) ; SV.toList a @?= [Nil,Nil]
         , testCase "storable on size 1 shape" $ 
