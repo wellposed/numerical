8a83eb293990b00f9bd4b165ed9e0ea18360dada
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-05-03 17:47:36 -0400

    fixup whitespace in State.ReallyStrict

--- src/Control/NumericalMonad/State/ReallyStrict.hs
+++ src/Control/NumericalMonad/State/ReallyStrict.hs
@@ -1,21 +1,29 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE UnboxedTuples #-}
 
-module Control.Monad.State.ReallyStrict(State,get,put) where 
+module Control.MonadInterna.State.ReallyStrict(State,get,put,runState) where
 
-import qualified Control.Applicative as A 
+import qualified Control.Applicative as A
+
+
+{-#INLINE runState #-}
+runState :: State s a   -- ^state-passing computation to execute
+         -> s           -- ^initial state
+         -> (a, s)      -- ^return value and final state
+runState  = case (unwrap m) s  of  (# a,s #) -> (a,s)
 
 
 newtype State s a = MkState (s -> (# a , s #))
 
-unwrap (MkState m)=m 
+
+unwrap (MkState m)=m
 
 {-#INLINE get #-}
-get :: State s s 
+get :: State s s
 get = MkState (\ !s -> (# s,s #))
 
 {-# INLINE put #-}
-put :: s -> State s () 
+put :: s -> State s ()
 put = \ !st -> MkState (\_ -> (# (), st #))
 
 {-#INLINE modify #-}
@@ -24,25 +32,25 @@ modify = \ !f ->  (MkState (\ !s -> (# (), f s #)))
 
 instance Functor (State s) where
     {-#INLINE fmap #-}
-    fmap = \ !f !(MkState mf) -> MkState (\ !s -> case mf s of (# v, newS   #)-> (# f v,newS   #) )    
+    fmap = \ !f !(MkState mf) -> MkState (\ !s -> case mf s of (# v, newS   #)-> (# f v,newS   #) )
 
 instance A.Applicative (State s) where
     {-# INLINE pure  #-}
     pure = \ !v -> (MkState (\ !s -> (# v , s #)))
     {-# INLINE (<*>) #-}
-    (<*>)= \ !(MkState fm) !(MkState vm) -> 
-                MkState ( \ !s -> 
-                    case  fm s of 
-                        (# fv, ns #)-> 
-                            case vm s of 
+    (<*>)= \ !(MkState fm) !(MkState vm) ->
+                MkState ( \ !s ->
+                    case  fm s of
+                        (# fv, ns #)->
+                            case vm s of
                                 (# vv, nns #) -> (# fv vv, nns #) )
 instance Monad (State s) where
     {-# INLINE return #-}
-    return = A.pure 
+    return = A.pure
     {-# INLINE (>>=) #-}
-    (>>=) = \ !(MkState m)  !f -> 
-                MkState $! \ !s -> case  m s of 
-                    (# v, ns #) -> 
-                            case  f v of 
-                                (MkState fm)-> fm ns 
+    (>>=) = \ !(MkState m)  !f ->
+                MkState $! \ !s -> case  m s of
+                    (# v, ns #) ->
+                            case  f v of
+                                (MkState fm)-> fm ns
 
