7e2fe8980d2827679cb0e92bb9b5d927cefc600b
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-07-24 13:43:48 -0400

    starting basic matrix example utils

---
+++ src/Numerical/Matrix/Basic.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables#-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
+
+module Numerical.Matrix.Basic where
+{-
+note, the contents of this module will probably be completely relocated elsewhere
+at some point
+
+-}
+import Numerical.Array.Mutable as Mut
+
+
+--cleverDotProduct  :: (Address ~ MArrayAddress mvecL
+--                    , Address ~ MArrayAddress mvecR
+--                    ,Array mvecL (S Z) a
+--                    ,Array mvecR (S Z) a
+--                    ,Num a)=>
+
+
+
