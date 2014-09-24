ddb66ad8f702cac95e7b3ae78e5fb0f1e7d327fb
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-09-24 00:53:01 -0400

    add basic hlint file (not that HLINT is mostly useless, but it occasionally has some nice ideas). So basically, hlint will never be part of the CI process, but is handy for catching certain mechanical cleanups

---
+++ HLint.hs
@@ -0,0 +1,13 @@
+import "hint" HLint.HLint
+
+ignore "Reduce duplication"
+ignore "Redundant lambda"
+ignore "Use first"
+ignore "Use second"
+ignore "Use import/export shortcut"
+ignore "Use camelCase"
+--ignore "Use >=>"
+--ignore "Use const"
+
+
+infixr 3 :*
